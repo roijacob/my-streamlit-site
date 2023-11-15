29c05135bd168bda2d83d967285cb2ba287147da
roijacob <roijac2b@outlook.com>
2023-11-15 23:02:12 +0800

    Implement final touches to complete website

--- app.py
+++ app.py
@@ -1,31 +1,39 @@
 import streamlit as st
 
-
 # Set streamlit page configurations
 st.set_page_config(layout="wide", 
                    initial_sidebar_state="collapsed")
 
 # Add Custom CSS to the streamlit page
-with open("assets/app.css", "r") as f:
-    page_bg_img = f.read()    
+try:
+    with open("app.css", "r") as f:
+        page_bg_img = f.read()    
+    st.markdown(page_bg_img, unsafe_allow_html=True)
+except FileNotFoundError:
+    st.error("File 'app.css' not found. Ensure it's in the correct directory.")
 
-st.markdown(page_bg_img, unsafe_allow_html=True)
+# Function to read file content
+def read_file(file_path):
+    """Reads text from a file and returns it."""
+    try:
+        with open(file_path, 'r', encoding='utf-8') as file:
+            return file.read()
+    except FileNotFoundError:
+        return f"Error: The file at '{file_path}' was not found."
 
 # Contents for the Home page
-
 col1, col2 = st.columns(2)
 
+# Column 1: About Me and Site Disclaimer
 with col1:
-    with open('assets/about_me.txt', 'r', encoding='utf-8') as file:
-        about_me_text = file.read()
-    st.write(about_me_text)
+    about_me = read_file('text_files/about_me.txt')
+    st.write(about_me)
+    site_disclaimer = read_file('text_files/html_css.txt')
+    st.markdown(site_disclaimer)
 
-    with open('assets/html_css.txt', 'r', encoding='utf-8') as file:
-        disclaimer = file.read()
-    st.markdown(disclaimer)
-
+# Column 2: Additional Information and Links
 with col2:
     st.header('More Information:', divider='rainbow')
     st.link_button("Download My Resume", "https://drive.google.com/uc?export=download&id=15FttVlCRlPaoxG8vFBVzsp_aUStltZfY", type="primary")
     st.link_button("My Projects", "projects", type="secondary")
-    st.link_button("LinkedIn", "www.linkedin.com/in/roi-jacob-olfindo-432402239", type="secondary")
+    st.link_button("LinkedIn", "https://www.linkedin.com/in/roi-jacob-olfindo-432402239", type="secondary")
--- pages/projects.py
+++ pages/projects.py
@@ -1,40 +1,57 @@
 import streamlit as st
 import streamlit.components.v1 as components
 
+def set_streamlit_config():
+    """Set Streamlit page configuration."""
+    st.set_page_config(layout="wide", initial_sidebar_state="collapsed")
 
-# Set streamlit page configurations
-st.set_page_config(layout="wide", 
-                   initial_sidebar_state="collapsed")
-
-# Add Custom CSS to the streamlit page
-with open("assets/app.css", "r") as f:
-    page_bg_img = f.read()    
-
-st.markdown(page_bg_img, unsafe_allow_html=True)
-
-# Contents for the Project page
-
-
-
-st.header('My Projects', divider='rainbow')
-
-
-def load_html_file(path):
-    with open(path, 'r', encoding='utf-8') as file:
+def read_file(file_path):
+    """Read content from a file."""
+    with open(file_path, 'r', encoding='utf-8') as file:
         return file.read()
 
-ev_wa_tableau_viz = load_html_file('./tableau_embed/ev_washington.html')
-vscode_tableau_viz = load_html_file('./tableau_embed/vs_code.html')
-phbanks_tableau_viz = load_html_file('./tableau_embed/ph_banks.html')
+def load_projects(file_names):
+    """Load project descriptions from files."""
+    projects = {}
+    for file_name in file_names:
+        project_key = file_name.split('_')[0]
+        projects[project_key] = read_file(f'text_files/{file_name}')
+    return projects
 
+def display_project(header, project_desc, tableau_viz):
+    """Display a project section."""
+    st.subheader(header, divider='rainbow')
+    st.write(project_desc)
+    components.html(tableau_viz)
+    st.markdown("")
 
-st.write('Ohhh, heart, heart, heart!!')
+# Set up the Streamlit page
+set_streamlit_config()
 
-# Project 1: Washington Vehicle Electrification 
-st.subheader('Electric Touch')
-components.html(ev_wa_tableau_viz)
+# Add custom CSS
+custom_css = read_file("app.css")
+st.markdown(custom_css, unsafe_allow_html=True)
 
+# Load project descriptions
+project_intro = read_file('text_files/project_description.txt')
+project_files = ['proj1_desc.txt', 'proj2_desc.txt', 'proj3_desc.txt']
+projects = load_projects(project_files)
 
+# Load Tableau visualizations
+ev_wa_tableau_viz = read_file('./tableau_embed/ev_washington.html')
+vscode_tableau_viz = read_file('./tableau_embed/vs_code.html')
+phbanks_tableau_viz = read_file('./tableau_embed/ph_banks.html')
 
-components.html(vscode_tableau_viz)
-components.html(phbanks_tableau_viz)
+# Display the Project page header
+st.header('My Projects', divider='rainbow')
+st.write(project_intro)
+
+# Display each project
+display_project('[Vehicle Electrification of Washington :flag-us:](https://public.tableau.com/views/WashingtonGoingElectric/Dashboard1?:language=en-US&:display_count=n&:origin=viz_share_link)', 
+                projects['proj1'], ev_wa_tableau_viz)
+
+display_project('[Demographics of Visual Studio Code Users :computer:](https://public.tableau.com/views/TheCultofVisualStudioCode-2/Dashboard1?:language=en-US&:display_count=n&:origin=viz_share_link)', 
+                projects['proj2'], vscode_tableau_viz)
+
+display_project('[How :flag-ph: :bank: Could Improve their :apple: Application](https://public.tableau.com/views/TheCultofVisualStudioCode-2/Dashboard1?:language=en-US&:display_count=n&:origin=viz_share_link)', 
+                projects['proj3'], phbanks_tableau_viz)
